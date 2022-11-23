ddc6557ba9a6ebc10a3c131062201d6ea0b0c347
Leonardo Larrad <leonardolarrad@gmail.com>
2022-11-23 00:50:19 -0400

    Enable proxy

--- src/ui/auth/login-page.js
+++ src/ui/auth/login-page.js
@@ -72,7 +72,7 @@ export default function LoginPage() {
       isLoading: true,
     });      
 
-    fetch('/api/auth/login', {
+    fetch('api/auth/login', {
       method: 'POST',
       mode: 'cors',
       headers: {
