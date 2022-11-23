2e7618892587c246e0db33bfacfd28c64fe058ee
Leonardo Larrad <leonardolarrad@gmail.com>
2022-11-23 00:11:13 -0400

    Enable CORS

--- src/ui/auth/login-page.js
+++ src/ui/auth/login-page.js
@@ -76,9 +76,7 @@ export default function LoginPage() {
       method: 'POST',
       mode: 'cors',
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',     
+        'Content-Type': 'application/json',  
       },
       body: JSON.stringify({
         email: form.email,
